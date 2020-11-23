d9d584daca5342e8f5187bb612643676d96c7c00
RhyanSPisoni <rhyanoficial14@gmail.com>
2020-11-23 09:01:52 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
-import { ReactiveFormsModule } from '@angular/forms'
-
-//Importações do Angular Material
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatInputModule} from '@angular/material/input';
-import { MatTabsModule } from '@angular/material/tabs'
-import { MatTableModule } from '@angular/material/table'
-import { MatIconModule } from '@angular/material/icon'
-import { MatCardModule } from '@angular/material/card'
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ClienteComponent } from './cliente/cliente.component';
-import { ClienteService } from './cliente.service'
+
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
+import { ClientesModule } from './clientes/clientes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClienteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatInputModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    MatTableModule,
-    MatTabsModule,
-    MatIconModule,
-    MatCardModule,
-    MatSnackBarModule,
-    BrowserAnimationsModule
+    TemplateModule,
+    ClientesModule
   ],
-  providers: [
-    ClienteService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
