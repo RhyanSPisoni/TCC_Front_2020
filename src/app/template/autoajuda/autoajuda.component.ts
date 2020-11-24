11fc2d70762f709d406c30d124cdd2cc28e89b7c
RhyanSPisoni <rhyanoficial14@gmail.com>
2020-11-24 10:16:52 -0300

    Att_Categorias

---
+++ src/app/template/autoajuda/autoajuda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autoajuda',
+  templateUrl: './autoajuda.component.html',
+  styleUrls: ['./autoajuda.component.css']
+})
+export class AutoajudaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { AcaoComponent } from './acao/acao.component';
+import { AventuraComponent } from './aventura/aventura.component';
+import { AdmComponent } from './adm/adm.component';
+import { AutoajudaComponent } from './autoajuda/autoajuda.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    AcaoComponent,
+    AventuraComponent,
+    AdmComponent,
+    AutoajudaComponent
   ],
   imports: [
     CommonModule,
@@ -16,7 +24,12 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    AcaoComponent,
+    AventuraComponent,
+    AdmComponent,
+    AutoajudaComponent
+
   ]
 })
 export class TemplateModule { }
