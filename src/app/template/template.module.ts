8a11b3e26af13ec116d00165f256d414f5478cd5
RhyanSPisoni <rhyanoficial14@gmail.com>
2020-11-24 14:31:43 -0300

    a

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -3,20 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
-import { AcaoComponent } from './acao/acao.component';
-import { AventuraComponent } from './aventura/aventura.component';
-import { AdmComponent } from './adm/adm.component';
-import { AutoajudaComponent } from './autoajuda/autoajuda.component';
-
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SidebarComponent,
-    AcaoComponent,
-    AventuraComponent,
-    AdmComponent,
-    AutoajudaComponent
+    SidebarComponent
   ],
   imports: [
     CommonModule,
@@ -24,12 +15,7 @@ import { AutoajudaComponent } from './autoajuda/autoajuda.component';
   ],
   exports: [
     NavbarComponent,
-    SidebarComponent,
-    AcaoComponent,
-    AventuraComponent,
-    AdmComponent,
-    AutoajudaComponent
-
+    SidebarComponent
   ]
 })
 export class TemplateModule { }
