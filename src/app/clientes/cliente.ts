c8efef2f90975a031bc9be5efa1371c1c95b4aa8
RhyanSPisoni <rhyanoficial14@gmail.com>
2020-11-25 11:48:24 -0300

    Cliente-form

--- src/app/clientes/cliente.ts
+++ src/app/clientes/cliente.ts
@@ -6,12 +6,4 @@ export class Cliente {
     senha: string;
     carteira: number;
     compras: number;      
-
-    constructor(nome: string, email: string, senha: string, carteira: number, compras: number){
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-        this.carteira = carteira;
-        this.compras = compras;
-    }
 }
