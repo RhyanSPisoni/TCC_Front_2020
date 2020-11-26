f301116d524c10b9b9c0c7e13a5344921694396d
brunogomes970 <73666147+brunogomes970@users.noreply.github.com>
2020-11-26 10:22:00 -0300

    cs

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Cliente } from './clientes/cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,12 @@ import { Injectable } from '@angular/core';
 export class ClientesService {
 
   constructor() { }
+
+  getCliente() : Cliente{
+    let cliente : Cliente = new Cliente();
+    cliente.nome ='Bruno';
+    cliente.cpf ='12345678910';
+    return cliente;
+
+  }
 }
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Cliente } from '../cliente'
+import{ ClientesService} from '../../clientes.service'
 
 @Component({
   selector: 'app-clientes-form',
