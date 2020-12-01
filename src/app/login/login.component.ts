a3fdfdbff2bee01a73daa87541894b88654a25c4
RhyanSPisoni <rhyanoficial14@gmail.com>
2020-12-01 12:51:21 -0300

    aa

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,38 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
 import { Usuario } from './usuario'
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent{
+export class LoginComponent  {
 
-  nome: string;
-  senha: string;
-  loginError: boolean;
+  username: string;
+  password: string;
   cadastrando: boolean;
-  mensagemSucesso: String;
+  mensagemSucesso: string;
+  errors: String[];
 
   constructor(
     private router: Router,
     private authService: AuthService
   ) { }
 
-  onSubmit(){
-    this.router.navigate(['/home'])
+  /*onSubmit(){
+    this.authService
+          .tentarLogar(this.username, this.password)
+          .subscribe(response => {
+            const access_token = JSON.stringify(response);
+            localStorage.setItem('access_token', access_token)
+            this.router.navigate(['/home'])
+          }, errorResponse => {
+            this.errors = ['Usuário e/ou senha incorreto(s).']
+          })
   }
+  */
 
   preparaCadastrar(event){
     event.preventDefault();
@@ -35,17 +44,21 @@ export class LoginComponent{
   }
 
   cadastrar(){
-    const usuario: Usuario = new Usuario()
-    usuario.username = this.nome;
-    usuario.password = this.senha;
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
     this.authService
-                    .salvar(usuario)
-                    .subscribe( response => {
-                      this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o Login.";
-                      this.loginError = false;
-                    }, error => {
-                      this.loginError = true;
-                      this.mensagemSucesso = null;
-                    })
+        .salvar(usuario)
+        .subscribe( response => {
+            this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login.";
+            this.cadastrando = false;
+            this.username = '';
+            this.password = '';
+            this.errors = []
+        }, errorResponse => {
+            this.mensagemSucesso = null;
+            this.errors = errorResponse.error.errors;
+        })
   }
+
 }
